28dd755a6360121a66be0cee9ab6e168066d5e5d
Achim <acch@users.noreply.github.com>
2015-07-27 20:53:41 +0200

    Parse bootstrap variables.less and config.json alike

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,23 +119,51 @@ module.exports = function(grunt) {
     // Recurse samples directory
     grunt.file.recurse('src/sample', function(abspath, rootdir, subdir, filename) {
       var destfile = path.join('src', filename);
+
       // Check if destination file exists
       if (! grunt.file.exists(destfile)) {
         grunt.log.writeln('Copying sample: ' + filename);
+
         // Copy sample to destination
         grunt.file.copy(abspath, destfile);
       }
     });
   });
 
-  // Read bootstrap config.json
+  // Read bootstrap variables.less or config.json
   grunt.registerTask('parse_bootstrap_config', function() {
-    var bootstrap_config = grunt.file.readJSON(path.join(bootstrap_path, 'config.json'));
+    // Find variables.less in bootstrap_path
+    var bootstrap_config_path = grunt.file.expand({ cwd: bootstrap_path }, '**/variables.less')[0];
+    if (bootstrap_config_path) {
+      grunt.log.writeln('Copying Bootstrap variables: ' + bootstrap_config_path);
+
+      // Copy variables.less
+      grunt.file.copy(path.join(bootstrap_path, bootstrap_config_path), 'less/variables.less');
+
+      return true;
+    }
+    grunt.log.writeln('Bootstrap variables not found!');
+
+    // Find config.json in bootstrap_path
+    var bootstrap_config_path = grunt.file.expand({ cwd: bootstrap_path }, '**/config.json')[0];
+    if (! bootstrap_config_path) {
+      grunt.log.error('Bootstrap config not found!');
+      
+      return false;
+    }
+    grunt.log.writeln('Parsing Bootstrap config: ' + bootstrap_config_path);
+
+    // Parse config.json
+    var bootstrap_config = grunt.file.readJSON(path.join(bootstrap_path, bootstrap_config_path));
     var variables = '';
     for (var key in bootstrap_config.vars) {
       variables += key + ':\t' + bootstrap_config.vars[key] + ';\n';
     }
+
+    // Generate variables.less
     grunt.file.write('less/variables.less', variables);
+
+    return true;
   });
 
   // Default task including everything
