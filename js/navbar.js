cddc18c4be2aa01d47bd7029f62bc9169054a7e8
Achim <acch@users.noreply.github.com>
2015-10-25 21:30:25 +0100

    Add click events to sidebar nav links for making Headroom pin

--- js/navbar.js
+++ js/navbar.js
@@ -7,4 +7,19 @@
 
   // initialize Headroom
   headroom.init();
+
+  // add click events to sidebar nav links
+  document.addEventListener("DOMContentLoaded", function() {
+    // choose elements to add click event to
+    var elements = document.getElementsByClassName("x2b-sdbr-lnk");
+
+    // iterate over elements
+    for (var i = 0; elements[i]; ++i) {
+      // add click event
+      elements[i].onclick = function() {
+        // fudge scroll position to make Headroom pin
+        headroom.lastKnownScrollY = headroom.getScrollerHeight();
+      }
+    }
+  });
 })();
