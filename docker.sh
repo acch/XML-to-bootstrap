7694d4d022cba1ab6001fb7b8e8568fd11b61443
Achim <acch@users.noreply.github.com>
2016-05-28 14:39:36 +0200

    Don't use cache when building image

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 # Stop an remove all containers
-docker stop x2b-test-1 
+docker stop x2b-test-1
 docker rm x2b-test-1
 
 # Delete image
@@ -9,7 +9,7 @@ docker rm x2b-test-1
 #docker images --filter dangling=true -q | xargs docker rmi
 
 # Build image based on Dockerfile
-docker build -t x2b .
+docker build --no-cache -t x2b .
 
 # Run container based on image
 docker run --name x2b-test-1 -d -p 8000:80 x2b
