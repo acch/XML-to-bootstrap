c18cab76264f1adb0f03e1e2bb170ebe399fe63d
Achim Christ <acch@users.noreply.github.com>
2017-07-04 16:48:32 +0200

    Use grunt init task for rebuilding Bootstrap

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -11,9 +11,7 @@ git checkout master # Still needed?
 #git pull --rebase # Not needed any longer?
 git checkout v4.0.0-alpha.6
 
-# Link and import our custom variables
-echo '@import "customvars";' >> scss/_custom.scss
-ln -s ../../../sass/customvars.scss scss/
+cd ../..
 
-# Rebuild Bootstrap
-npm install && grunt dist
+# Link and import our custom variables, rebuild Bootstrap
+grunt init
